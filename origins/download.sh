3ed219da8ab7c2bd73b4a4d8002a5d35adeb7d19
iltars <Ilya-Tar2003@yandex.ru>
2026-03-22 02:42:53 +0700

    data_preprocessing

--- origins/download.sh
+++ origins/download.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-curl -L -o 31Kmeta_en.zip\
-  https://www.kaggle.com/api/v1/datasets/download/Cornell-University/arxiv
-unzip 31Kmeta_en.zip -d ./31Kmeta_en
-rm 31Kmeta_en.zip
+# curl -L -o 3Mmeta_en.zip\
+#   https://www.kaggle.com/api/v1/datasets/download/Cornell-University/arxiv
+# unzip 3Mmeta_en.zip -d ./3Mmeta_en
+# rm 3Mmeta_en.zip
 
-curl -L -o 24Kmeta_en.zip\
+curl -L -o 41Kmeta_en.zip\
   https://www.kaggle.com/api/v1/datasets/download/neelshah18/arxivdataset
-unzip 24Kmeta_en.zip -d ./24Kmeta_en
-rm 24Kmeta_en.zip
+unzip 24Kmeta_en.zip -d ./41Kmeta_en
+rm 41Kmeta_en.zip
