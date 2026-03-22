f6ae36b87bfa60d5ee769c4ac4c534223fbbaa2e
iltars <Ilya-Tar2003@yandex.ru>
2026-03-22 21:15:44 +0700

    teacher

--- origins/download.sh
+++ origins/download.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-# curl -L -o 3Mmeta_en.zip\
-#   https://www.kaggle.com/api/v1/datasets/download/Cornell-University/arxiv
-# unzip 3Mmeta_en.zip -d ./3Mmeta_en
-# rm 3Mmeta_en.zip
+curl -L -o 3Mmeta_en.zip\
+  https://www.kaggle.com/api/v1/datasets/download/Cornell-University/arxiv
+unzip 3Mmeta_en.zip -d ./3Mmeta_en
+rm 3Mmeta_en.zip
 
 curl -L -o 41Kmeta_en.zip\
   https://www.kaggle.com/api/v1/datasets/download/neelshah18/arxivdataset
